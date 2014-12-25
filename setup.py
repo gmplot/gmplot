fda1e7a14a71065c6e69e734009d491859b8a44e
Michael Woods <physicsmichael@gmail.com>
2014-12-25 00:10:17 -0800

    Initial setup.py.

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from distutils.core import setup
+
+__version__ = '0.0.1'
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'gmplot',
+    version = __version__,
+    author = 'Michael Woods',
+    author_email = 'physicsmichael@gmail.com',
+    py_modules = ['gmplot'],
+    description = 'Provide a matplotlib like interface to plotting data with Google Maps',
+    long_description=read('README.rst'),
+    license='MIT',
+    keywords='python wrapper google maps',
+
+)
