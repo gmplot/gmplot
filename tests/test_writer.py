75bf755729a2993f369af8d8a3e5d55b1da273af
Faris Lama <fy.lama@gmail.com>
2020-05-23 14:47:55 -0400

    Reintroduce StringIO for both Python 2 and 3.
    This enables us to maintain a single testable `_Writer` class instead of separate `_StringWriter` and `_FileWriter` classes.

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,6 +1,7 @@
 import unittest
 import warnings
-from gmplot.writer import _StringWriter
+from gmplot.utility import StringIO
+from gmplot.writer import _Writer
 
 def _get_comparison_error_message(output, expected_output):
     return """
@@ -11,15 +12,17 @@ Doesn't match the expected output:
 '''%s'''
 """ % (output, expected_output)
 
-class StringWriterTest(unittest.TestCase):
+class WriterTest(unittest.TestCase):
     def test_writing_lines(self):
-        writer = _StringWriter()
-        writer.write("Here's a sample line...").write()
-        writer.write('List of random items:')
-        writer.indent()
-        writer.write('- First')
-        writer.write('- Second')
-        writer.write('- Third')
+        with StringIO() as f:
+            with _Writer(f) as writer:
+                writer.write("Here's a sample line...").write()
+                writer.write('List of random items:')
+                writer.indent()
+                writer.write('- First')
+                writer.write('- Second')
+                writer.write('- Third')
+            output_string = f.getvalue()
 
         EXPECTED_OUTPUT = '''\
 Here's a sample line...
@@ -30,50 +33,52 @@ List of random items:
     - Third
 '''
 
-        output_string = writer.get()
         self.assertEqual(output_string, EXPECTED_OUTPUT, _get_comparison_error_message(output_string, EXPECTED_OUTPUT))
 
     def test_writing_multilines(self):
-        writer = _StringWriter()
-        writer.write('''
+        with StringIO() as f:
+            with _Writer(f) as writer:
+                writer.write('''
 
 
-            Here's a sample line...
+                    Here's a sample line...
 
-            List of random items:
-                - Table
-            \t- Second
-                - Third
+                    List of random items:
+                        - First
+                    \t- Second
+                        - Third
 
 
-        ''')
+                ''')
+            output_string = f.getvalue()
 
         EXPECTED_OUTPUT = '''\
 Here's a sample line...
 
 List of random items:
-    - Table
+    - First
     - Second
     - Third
 '''
 
-        output_string = writer.get()
         self.assertEqual(output_string, EXPECTED_OUTPUT, _get_comparison_error_message(output_string, EXPECTED_OUTPUT))
 
     def test_extra_dedentation(self):
-        writer = _StringWriter()
-        writer.indent().write("Here's a sample indented line...")
-        writer.dedent().write("Here's another line before an extra dedent...")
+        with StringIO() as f:
+            with _Writer(f) as writer:
+                writer.indent().write("Here's a sample indented line...")
+                writer.dedent().write("Here's another line before an extra dedent...")
 
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
+                with warnings.catch_warnings(record=True) as w:
+                    warnings.simplefilter("always")
 
-            writer.dedent()
+                    writer.dedent()
 
-            self.assertEqual(len(w), 1, "The extra dedent should raise a single warning")
-            self.assertTrue(issubclass(w[-1].category, UserWarning), "The extra dedent should raise a 'UserWarning'")
+                    self.assertEqual(len(w), 1, "The extra dedent should raise a single warning")
+                    self.assertTrue(issubclass(w[-1].category, UserWarning), "The extra dedent should raise a 'UserWarning'")
 
-        writer.write("And here's a third line after the extra dedent.")
+                writer.write("And here's a third line after the extra dedent.")
+            output_string = f.getvalue()
 
         EXPECTED_OUTPUT = '''\
     Here's a sample indented line...
@@ -81,5 +86,14 @@ Here's another line before an extra dedent...
 And here's a third line after the extra dedent.
 '''
 
-        output_string = writer.get()
         self.assertEqual(output_string, EXPECTED_OUTPUT, _get_comparison_error_message(output_string, EXPECTED_OUTPUT))
+
+    def test_exception_exit(self):
+        with StringIO() as f:
+            try:
+                with _Writer(f) as writer:
+                    writer.write('Content')
+                    raise Exception
+                    writer.write('Unreachable content')
+            except:
+                self.assertEqual(f.getvalue(), '', "The written-to file should be empty if an exception is triggered")
