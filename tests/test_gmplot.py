d92773ac99c322d64344b927958356de8f11bba4
Faris Lama <fy.lama@gmail.com>
2020-05-24 13:50:58 -0400

    Add `_format_LatLng()` unit test.

--- tests/test_gmplot.py
+++ tests/test_gmplot.py
@@ -1,5 +1,12 @@
 import unittest
 import gmplot
+from gmplot.gmplot import _format_LatLng
+
+class GMPlotTest(unittest.TestCase):
+    def test_format_LatLng(self):
+        self.assertEqual(_format_LatLng(45.123456, -80.987654), 'new google.maps.LatLng(45.123456, -80.987654)')
+        self.assertEqual(_format_LatLng(45.123456, -80.987654, 4), 'new google.maps.LatLng(45.1235, -80.9877)')
+        self.assertEqual(_format_LatLng(45.1, -80.9, 3), 'new google.maps.LatLng(45.100, -80.900)')
 
 class TestMarkers(unittest.TestCase):
     def setUp(self):
