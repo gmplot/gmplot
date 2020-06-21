0dee8d76df0a2fbf74cfb431486c93634ab78cf6
Faris Lama <fy.lama@gmail.com>
2020-06-20 23:06:04 -0400

    Update version to 1.3.0.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
