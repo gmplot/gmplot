bd25fac6c680c824d9add0f508c7496f125210e0
Faris Lama <fy.lama@gmail.com>
2020-07-01 19:25:46 -0400

    Refactor circle and symbol drawing and have them use kwargs.

---
+++ gmplot/drawables/symbol.py
@@ -0,0 +1,62 @@
+from gmplot.color import _get_hex_color
+from gmplot.utility import _get_value
+
+from gmplot.drawables.symbols.circle import _Circle
+from gmplot.drawables.symbols.plus import _Plus
+from gmplot.drawables.symbols.x import _X
+
+class _Symbol(object):
+    _SHAPES = {
+        'o': _Circle,
+        '+': _Plus,
+        'x': _X
+    }
+
+    @staticmethod
+    def is_valid(shape):
+        '''
+        Return whether or not a shape is valid.
+
+        Args:
+            shape (str): Shape of a symbol.
+        
+        Returns:
+            bool: True if the shape is valid, False otherwise.
+        '''
+        return shape in _Symbol._SHAPES
+
+    def __init__(self, shape, lat, lng, size, **kwargs): # TODO: Pass in precision as well.
+        '''
+        Args:
+            shape (str): Shape of the symbol, as 'o', 'x', or '+'.
+            lat (float): Latitude of the center of the symbol.
+            lng (float): Longitude of the center of the symbol.
+            size (int): Size of the symbol, in meters.
+
+        Optional:
+
+        Args:
+            edge_alpha/ea (float): Opacity of the symbol's edge, ranging from 0 to 1. Defaults to 1.0.
+            edge_width/ew (int): Width of the symbol's edge, in pixels. Defaults to 1.
+            face_alpha/alpha (float): Opacity of the symbol's face, ranging from 0 to 1. Defaults to 0.5.
+            color/c/face_color/fc (str): Color of the symbol's face.
+                Can be hex ('#00FFFF'), named ('cyan'), or matplotlib-like ('c'). Defaults to black.
+            color/c/edge_color/ec (str): Color of the symbol's edge.
+                Can be hex ('#00FFFF'), named ('cyan'), or matplotlib-like ('c'). Defaults to black.
+        '''
+        kwargs.setdefault('edge_color', _get_hex_color(_get_value(kwargs, ['color', 'c', 'edge_color', 'ec'], '#000000')))
+        kwargs.setdefault('edge_alpha', _get_value(kwargs, ['edge_alpha', 'ea'], 1.0))
+        kwargs.setdefault('edge_width', _get_value(kwargs, ['edge_width', 'ew'], 1))
+        kwargs.setdefault('face_alpha', _get_value(kwargs, ['face_alpha', 'alpha'], 0.5))
+        kwargs.setdefault('face_color', _get_hex_color(_get_value(kwargs, ['color', 'c', 'face_color', 'fc'], '#000000')))
+
+        self._symbol = self._SHAPES[shape](lat, lng, size, **kwargs)
+
+    def write(self, w):
+        '''
+        Write the symbol.
+
+        Args:
+            w (_Writer): Writer used to write the symbol.
+        '''
+        self._symbol.write(w)
---
+++ gmplot/drawables/symbols/circle.py
@@ -0,0 +1,58 @@
+from gmplot.color import _get_hex_color
+from gmplot.utility import _get_value, _format_LatLng
+
+class _Circle(object):
+    def __init__(self, lat, lng, radius, **kwargs):
+        '''
+        Args:
+            lat (float): Latitude of the center of the circle.
+            lng (float): Longitude of the center of the circle.
+            radius (int): Radius of the circle, in meters.
+
+        Optional:
+
+        Args:
+            edge_alpha/ea (float): Opacity of the circle's edge, ranging from 0 to 1. Defaults to 1.0.
+            edge_width/ew (int): Width of the circle's edge, in pixels. Defaults to 1.
+            face_alpha/alpha (float): Opacity of the circle's face, ranging from 0 to 1. Defaults to 0.5.
+            color/c/face_color/fc (str): Color of the circle's face.
+                Can be hex ('#00FFFF'), named ('cyan'), or matplotlib-like ('c'). Defaults to black.
+            color/c/edge_color/ec (str): Color of the circle's edge.
+                Can be hex ('#00FFFF'), named ('cyan'), or matplotlib-like ('c'). Defaults to black.
+        '''
+        self._center = _format_LatLng(lat, lng) # TODO: Pass in precision as well.
+        self._radius = radius
+        self._edge_color = _get_hex_color(_get_value(kwargs, ['color', 'c', 'edge_color', 'ec'], '#000000'))
+        self._edge_alpha = _get_value(kwargs, ['edge_alpha', 'ea'], 1.0)
+        self._edge_width = _get_value(kwargs, ['edge_width', 'ew'], 1)
+        self._face_alpha = _get_value(kwargs, ['face_alpha', 'alpha'], 0.5)
+        self._face_color = _get_hex_color(_get_value(kwargs, ['color', 'c', 'face_color', 'fc'], '#000000'))
+
+    def write(self, w): # TODO: Set clickable=false and geodesic=true
+        '''
+        Write the circle.
+
+        Args:
+            w (_Writer): Writer used to write the circle.
+        '''
+        w.write('''
+            new google.maps.Circle({{
+                strokeColor: '{edge_color}',
+                strokeOpacity: {edge_alpha},
+                strokeWeight: {edge_width},
+                fillColor: '{face_color}',
+                fillOpacity: {face_alpha},
+                center: {center},
+                radius: {radius},
+                map: map
+            }});
+        '''.format(
+            edge_color=self._edge_color,
+            edge_alpha=self._edge_alpha,
+            edge_width=self._edge_width,
+            face_color=self._face_color,
+            face_alpha=self._face_alpha,
+            center=self._center,
+            radius=self._radius
+        ))
+        w.write()
